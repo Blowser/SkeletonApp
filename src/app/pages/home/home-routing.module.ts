e52d3e431d792e21dced88f12d57a2d2960a4756
Blowser <ig.cortez@duocuc.cl>
2024-12-03 21:49:43 -0300

    Update home-routing.module.ts
    anuncios

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { HomePage } from './home.page'; // Importa la página principal
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
-  }
+    component: HomePage, // Define la ruta base como el componente HomePage
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  imports: [
+    RouterModule.forChild(routes), // Importa RouterModule para las rutas definidas
+  ],
+  exports: [
+    RouterModule, // Exporta RouterModule para que esté disponible en otros módulos
+  ],
 })
 export class HomePageRoutingModule {}
